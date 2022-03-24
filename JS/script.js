ea61381256a8826ecc7bb75d6f50c006f187c41c
ron-thecertifiedbomb <99375281+ron-thecertifiedbomb@users.noreply.github.com>
2022-03-24 14:59:16 +0800

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -7,7 +7,7 @@ function myFunction() {
     const addNew = document.getElementById("myText");
     const para = document.createElement("p");
     const rad = document.createElement("img");
-    rad.setAttribute("src", "red.PNG");
+    rad.setAttribute("src", "/assets/red.PNG");
     const leftContainer = document.getElementById("content-left");
     const righttContainer = document.getElementById("content-right");
     
