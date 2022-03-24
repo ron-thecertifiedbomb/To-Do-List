68ae8f1719c67dd05e00a1ce304d0ea17214fc0b
ron-thecertifiedbomb <99375281+ron-thecertifiedbomb@users.noreply.github.com>
2022-03-24 13:59:38 +0800

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -2,36 +2,12 @@
 
 document.getElementById("heading").innerHTML = "TO DO LIST";
 
-
-function myDynamic() {
-    const para = document.createElement("p");
-    const rad = document.createElement("img");
-
-    rad.setAttribute("src", "red.png");
-
-    para.innerText = "This is a radio button.";
-    document.getElementById("content-left").appendChild(rad);
-    document.getElementById("content-right").appendChild(para);
-
-    rad.addEventListener('click', function removeList() {
-        const rad = document.getElementById("content-left");
-        const para = document.getElementById("content-right");
-        rad.removeChild(rad.firstElementChild);
-        para.removeChild(para.firstElementChild);
-    })
-
-}
-
-
-
-
-
 function myFunction() {
 
     const addNew = document.getElementById("myText");
     const para = document.createElement("p");
     const rad = document.createElement("img");
-    rad.setAttribute("src", "red.png");
+    rad.setAttribute("src", "/assets/red.png");
     const leftContainer = document.getElementById("content-left");
     const righttContainer = document.getElementById("content-right");
     
